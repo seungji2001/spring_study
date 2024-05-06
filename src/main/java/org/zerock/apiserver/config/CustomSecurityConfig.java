77bd0970785079f39c47ed02b48c5a21c0ae10ea
Seungji Baek <seungjibaek@Seungjis-MacBook-Pro.local>
2024-05-06 16:52:18 +0900

    feat: 로그인 성공시 핸들러 사용

--- src/main/java/org/zerock/apiserver/config/CustomSecurityConfig.java
+++ src/main/java/org/zerock/apiserver/config/CustomSecurityConfig.java
@@ -7,12 +7,14 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.zerock.apiserver.security.handler.APILoginSuccessHandler;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -34,12 +36,24 @@ public class CustomSecurityConfig {
             httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource());
         });
 
+        //세션 못만들도록 만드는것
+        http.sessionManagement(httpSecuritySessionManagementConfigurer -> {
+            httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        });
+
+
+
+
+
         //csrf설정 -> request 위조 방지
         http.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
 
         // 폼 로그인을 활성화하고, 로그인 페이지를 /api/member/login으로 설정합니다. 이 설정은 사용자가 로그인할 때 사용할 페이지를 지정합니다.
         http.formLogin(config -> {
+            //로그인 페이지
             config.loginPage("/api/member/login");
+            //로그인 성공시 핸들러 사용 -> 성공시 핸들러 사용
+            config.successHandler(new APILoginSuccessHandler());
         });
 
         //return http.build();: 구성된 HttpSecurity 설정을 바탕으로 SecurityFilterChain 객체를 빌드하고 반환합니다.
